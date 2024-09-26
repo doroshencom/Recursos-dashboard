0a6b0532467eb2447576be72e0d3b64d89e83e42
doroshencom <dimitridoroshenko@gmail.com>
2024-09-26 15:33:53 +0200

    nice changes, ahora es más visual

--- src/components/ResourceCard.js
+++ src/components/ResourceCard.js
@@ -7,12 +7,14 @@ function ResourceCard({ resource }) {
 
   return (
     <div className={`resource-card ${isExpanded ? 'expanded' : ''}`} onClick={toggleExpand}>
-      <div className="resource-header">
+      <div className="resource-img">
         <img
           src={isExpanded ? resource.imagenGrande : resource.miniatura || "https://img.freepik.com/vector-gratis/ilustracion-icono-galeria_53876-27002.jpg"}
           alt="Miniatura"
           className={`resource-thumbnail ${isExpanded ? 'expanded-thumbnail' : ''}`}
         />
+      </div>
+      <div className="resource-header">
         <div>
           <h3>{resource.titulo}</h3>
           {!isExpanded && (
@@ -24,9 +26,15 @@ function ResourceCard({ resource }) {
           )}
         </div>
       </div>
+
       {isExpanded && (
         <div className="resource-details">
           <p>{resource.descripcion || "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at ipsum ut sem sollicitudin."}</p>
+          <div className="resource-tags">
+              {resource.etiquetas.map((tag, index) => (
+                <span key={index} className="tag">{tag}</span>
+              ))}
+            </div>
           <a href={resource.enlace} className="resource-link">Leer</a>
         </div>
       )}
